9aa724be2fc2b40bd955532fcb918a1e15a57118
Posccis <lkzq33@gmail.com>
2023-11-25 13:16:43 -0300

    new(main)= Aula de criação de componentes.

---
+++ src/components/Comentario.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Comentario.css'
+
+const Comentario = () => (
+    <>
+        <a className='comentario'>Olá eu sou um component</a>
+        <div>
+            <a>Teste</a>
+        </div>
+    </>
+);
+
+export default Comentario;
